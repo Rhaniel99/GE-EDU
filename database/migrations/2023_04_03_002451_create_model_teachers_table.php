c8f0c4118532c83b2b814be6a5a585172934c1f3
rhaniel99 <rhanielmonteiro.13@gmail.com>
2023-04-02 20:44:03 -0400

    Segundo commit

---
+++ database/migrations/2023_04_03_002451_create_model_teachers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teacher', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('mat')->unique();
+            $table->timestamp('graduate')->nullable();
+            $table->string('s');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teacher');
+    }
+};
